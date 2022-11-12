5e1a6feb311c7242df07f3b8dbc41005a2dbb5cd
Nelli Gaifullina <nellie.mailbox@gmail.com>
2022-11-12 14:41:41 +0300

    common elements

---
+++ src/commonElements.ts
@@ -0,0 +1,16 @@
+const first = [1, 2, 3, 4];
+const second = [3, 4, 5, 6];
+
+function common(a: Array<number>, b: Array<number>) {
+  let obj: Record<number, number> = {};
+  let commonArr: Array<number> = [];
+  a.map((el) => (obj[el] = el));
+  for (let i = 0; i <= b.length; i++) {
+    if (obj[b[i]]) {
+      commonArr.push(b[i]);
+    }
+  }
+  return commonArr;
+}
+
+console.log(common(first, second));
