33e4c3c1828aa76eaf41433f08caaf1631f7eb91
Nelli Gaifullina <nellie.mailbox@gmail.com>
2023-01-18 07:45:53 +0300

    sleep

--- src/timeout/lib/sleep.ts
+++ src/timeout/lib/sleep.ts
@@ -1,4 +1,17 @@
+//todo
 export const sleep = (milliseconds: number): Promise<number> =>
   new Promise((resolve) =>
     setTimeout(() => resolve(milliseconds), milliseconds)
   );
+
+const timer1 = setTimeout(() => {
+  console.log("timer1");
+
+  const promise1 = Promise.resolve().then(() => {
+    console.log("promise1");
+  });
+}, 0);
+
+const timer2 = setTimeout(() => {
+  console.log("timer2");
+}, 0);
