9cbfd5befb3a4d993917a3e31f0e63924bed0fdf
Nelli Gaifullina <nellie.mailbox@gmail.com>
2023-01-26 08:22:42 +0300

    code format

--- src/popup/script.js
+++ src/popup/script.js
@@ -4,21 +4,16 @@ const userName = document.querySelector("#nameToChange");
 const userJob = document.querySelector("#jobToChange");
 const inputName = document.querySelector("#name");
 const inputJob = document.querySelector("#job");
-const pageCover = document.querySelector(".main_cover");
-const modal = document.querySelector(".main__form");
-console.log(button, pageCover, modal);
+const modal = document.querySelector(".main_cover");
 
 button.addEventListener("click", () => {
   console.log("in click");
-  // pageCover.classList.add("opacity");
   modal.classList.add("visible");
-
-  modalWindow.classList.add("visible");
 });
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   userName.textContent = inputName.value;
   userJob.textContent = inputJob.value;
-  modalWindow.classList.remove("visible");
+  modal.classList.remove("visible");
 });
