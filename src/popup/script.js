227c2679ab976b73e99e6ee37cd311710cd19afd
Nelli Gaifullina <nellie.mailbox@gmail.com>
2023-01-31 09:10:36 +0300

    form addField

--- src/popup/script.js
+++ src/popup/script.js
@@ -33,13 +33,18 @@ class Form {
 
   // todo arguments
   addField(id) {
-    plusButton.insertAdjacentHTML(
-      "beforebegin",
+    const div = element(
       `<div class="form__raw">
-    <input type="text" name="" id=${id}/>
-    <label>${id}</label>
-  </div>`
+        <input type="text" id=${id}/>
+        <label>${id}</label>
+      </div>`
     );
+
+    div.addEventListener("input", (e) => {
+      console.log(`input id:${id} changed to:${e.target.value}`);
+    });
+
+    plusButton.insertAdjacentElement("beforebegin", div);
   }
 
   deleteField() {
@@ -52,6 +57,17 @@ class Form {
   changeFieldValue() {}
 }
 
+/**
+ * @param {String} HTML representing a single element
+ * @return {Element}
+ */
+function element(html) {
+  var template = document.createElement("template");
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
 const button = document.querySelector("button");
 const form = document.querySelector("form");
 const modal = document.querySelector(".main_cover");
@@ -62,7 +78,7 @@ const minusButton = document.querySelector(".form__minus");
 const data = [
   {
     id: "Name",
-    model: new Model(""),
+    model: new Model({ option: "", optionValue: "" }),
     callback: (newValue) =>
       (document.querySelector("#nameToChange").textContent = newValue),
   },
@@ -80,15 +96,19 @@ button.addEventListener("click", () => {
 
 plusButton.addEventListener("click", () => formModel.addField("Job"));
 minusButton.addEventListener("click", () => formModel.deleteField());
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const option = form.option.value;
+  const optionValue = document.getElementsByName(option)[0].value;
+  console.log(option, "option");
 
-  data
-    .filter((el) => document.querySelector(`#${el.id}`).value)
-    .forEach((el) => {
-      el.model.onChange(el.callback);
-      el.model.set(document.querySelector(`#${el.id}`).value);
-    });
+  // data
+  //   .filter((el) => document.querySelector(`#${el.id}`).value)
+  //   .forEach((el) => {
+  //     el.model.onChange(el.callback);
+  //     el.model.set(document.querySelector(`#${el.id}`).value);
+  //   });
 
   modal.classList.remove("visible");
   form.reset();
