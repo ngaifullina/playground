de689b96bfe446a38b858108f197518b581fdcaf
Nelli Gaifullina <nellie.mailbox@gmail.com>
2023-04-11 20:47:28 +0400

    close popup on window click

--- src/html/script.ts
+++ src/html/script.ts
@@ -3,26 +3,44 @@ import { data } from "./lib/data.js";
 import type { FormState } from "./lib/form/types.js";
 
 const openFormButton = document.querySelector(".header__button")!;
-const formPopup = document.querySelector(".main_cover")!;
+const mainCover = document.querySelector(".main_cover")!;
 const closeFormButton = document.querySelector(".close")!;
+let hasBeenOpened = false;
 
 const onSubmit = (formState: FormState) => {
-  formPopup.classList.remove("visible");
+  mainCover.classList.remove("visible");
   formState.forEach((el) =>
     data.find((d) => d.name === el.option)!.callback(el.value)
   );
 };
 
 openFormButton.addEventListener("click", () => {
-  formPopup.classList.add("visible");
+  mainCover.classList.add("visible");
+  if (hasBeenOpened) return;
 
   openForm(
     document.querySelector(".popup")!,
     data.map(({ name }) => name),
     onSubmit
   );
+  hasBeenOpened = true;
 });
 
-closeFormButton.addEventListener("click", () =>
-  formPopup.classList.remove("visible")
-);
+// todo abstract away popup
+/*
+<div class="popup">
+  <div class="popup__overlay"></div>
+  <div class="popup__body">
+    <div class="popup__close"></div>
+  </div>
+</div>
+*/
+
+mainCover.addEventListener("click", ({ target }) => {
+  const clickedOutsidePopup = !(target as HTMLElement).closest(".popup");
+  const clickedOnCloseFormButton = target === closeFormButton;
+
+  if (clickedOutsidePopup || clickedOnCloseFormButton) {
+    mainCover.classList.remove("visible");
+  }
+});
